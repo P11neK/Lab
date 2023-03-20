fed419db86b21cf225aa200281600e20e163e78a
P11neK <zatylkinr@gmail.com>
2023-03-20 17:28:31 +0300

    Lab 7

--- lab6/Source.cpp
+++ lab6/Source.cpp
@@ -1,22 +1,57 @@
+/*Упорядочение массива в порядке убывания значений элементов	Умножение векторов*/
+
 #include <iostream>
 #include <iomanip>
 
 
+int const row = 4, col = 4;
+// Прототип
+void CreateMassivA(int massivA[]);
+void CreateMassivB(int massivA[], float massivB[][col]);
+void Print(float massivB[][col]);
+void CreateMassivB1(float massivB[row][col], float massivB1[row]);
+void PrintB1(float massivB1[]);
+void SortMassivA1(int massivA[row], int massivA1[row]);
+void SortMassivB2(float massivB1[row]);
+void CreateMassivC(float massivC[row], int massivA[row], float massivB1[row]);
 
 
 int main()
+
 {
-    int const row = 4, col = 4;
+
     int massivA[row], massivA1[row];
-    float massivB[row][col], massivB1[row], C = 0.0;
-    setlocale(LC_ALL, "rus");
-    /*Создание массива А*/
+    float massivB[row][col], massivB1[row], massivC[row];
+    setlocale(0, "rus");
+
+    CreateMassivA(massivA);
+    CreateMassivB(massivA, massivB);
+    Print(massivB);
+    CreateMassivB1(massivB, massivB1);
+    PrintB1(massivB1);
+    std::cout << std::endl;
+    SortMassivA1(massivA, massivA1);
+    SortMassivB2(massivB1);
+    CreateMassivC(massivC, massivA, massivB1);
+
+
+
+    system("pause");
+    return 0;
+
+}
+
+void CreateMassivA(int massivA[])
+{
     for (int i = 0; i < row; i++)
     {
-        std::cout << "a[" << i + 1 << "]=";
+        std::cout << "a[" << i + 1 << "]  ";
         std::cin >> massivA[i];
     }
-    /*Создание массива Б*/
+}
+
+void CreateMassivB(int massivA[], float massivB[][col])
+{
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -31,40 +66,10 @@ int main()
             }
         }
     }
+}
 
-    /*создание массива Б1*/
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            if (i == j)
-            {
-                massivB1[i] = massivB[i][j];
-            }
-        }
-    }
-
-
-    std::cout << "Массив А1(Массив А, отсортированный методом пузырька)" << std::endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = col - 1; j > i; j--)
-        {
-            if (massivA[j] > massivA[j - 1])
-            {
-                int prom = massivA[j];
-                massivA[j] = massivA[j - 1];
-                massivA[j - 1] = prom;
-            }
-        }
-        massivA1[i] = massivA[i]; /*заполнение массива А1*/
-        std::cout << massivA1[i] << std::setw(3); /*вывод массива А1*/
-    }
-    std::cout << std::endl;
-
-
-   /* Вывод результатов*/
+void Print(float massivB[][col])
+{
     std::cout << "Массив B";
     std::cout << "\n";
     for (int i = 0; i < row; i++)
@@ -75,43 +80,30 @@ int main()
         }
         std::cout << "\n";
     }
+}
 
 
-    std::cout << "Массив B1 (Массив из главных диагоналей массива В)";
-    std::cout << "\n";
-    for (int i = 0; i < 4; i++)
-    {
-        std::cout << massivB1[i] << std::setw(10);
-    }
-    std::cout << std::endl;
-
-    std::cout << "Массив B2(Массив B1, отсортированный методом пузырька)" << std::endl;
+void CreateMassivB1(float massivB[row][col], float massivB1[row])
+{
     for (int i = 0; i < row; i++)
     {
-        for (int j = col - 1; j > i; j--)
+        for (int j = 0; j < col; j++)
         {
-            if (massivB1[j] > massivB1[j - 1])
+            if (i == j)
             {
-                float prom = massivB1[j];
-                massivB1[j] = massivB1[j - 1];
-                massivB1[j - 1] = prom;
+                massivB1[i] = massivB[i][j];
             }
         }
-        std::cout << massivB1[i] << std::setw(10); /*вывод массива B2*/
     }
-    std::cout << std::endl;
-
-    std::cout << "Массив C(Результат от перемножения матриц А1 и В2)" << std::endl;
+}
+void PrintB1(float massivB1[])
+{
+    std::cout << "Массив B1";
+    std::cout << "\n";
     for (int i = 0; i < row; i++)
     {
-        C += (massivA[i] * massivB1[i]);
-
+        std::cout << std::setw(10) << massivB1[i];
     }
-    std::cout << C;
-    std::cout << std::endl;
-
-
-    system("pause");
-    return 0;
-
 }
+
+void SortMassivA1(int massivA[row], int massivA1[row])
